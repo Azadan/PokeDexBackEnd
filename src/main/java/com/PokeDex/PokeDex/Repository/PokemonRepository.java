56947dd67fe0ff39df4631e078808603c6ae9e0b
Azadan <azadan@live.com>
2025-01-30 21:37:41 +0100

    Issue #6: Repository

--- src/main/java/com/PokeDex/PokeDex/Repository/PokemonRepository.java
+++ src/main/java/com/PokeDex/PokeDex/Repository/PokemonRepository.java
@@ -1,5 +1,11 @@
 package com.PokeDex.PokeDex.Repository;
 
-public interface PokemonRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.PokeDex.PokeDex.API.Model.Pokemon;
+import java.util.List;
+
+
+public interface PokemonRepository extends JpaRepository<Pokemon,Long>{
+    //List<Pokemon> findById(long id);
 }

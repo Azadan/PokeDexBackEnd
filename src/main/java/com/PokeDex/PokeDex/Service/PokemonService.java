3ceb44d45091cdcaa61c74c706e71d98b85d7492
Azadan <azadan@live.com>
2025-01-30 21:40:50 +0100

    Issue #6: savePokemon method and postmap

--- src/main/java/com/PokeDex/PokeDex/Service/PokemonService.java
+++ src/main/java/com/PokeDex/PokeDex/Service/PokemonService.java
@@ -1,5 +1,23 @@
 package com.PokeDex.PokeDex.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.PokeDex.PokeDex.API.Model.Pokemon;
+import com.PokeDex.PokeDex.Repository.PokemonRepository;
+
+@Service
 public class PokemonService {
+
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    public PokemonService(PokemonRepository pokemonRepository) {
+        this.pokemonRepository = pokemonRepository;
+    }
     
+    public Pokemon savePokemon(Pokemon pokemon) {
+        return pokemonRepository.save(pokemon);
+    }
+
 }
